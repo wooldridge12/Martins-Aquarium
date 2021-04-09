90cbfa7951e2270b89422bdc48504690d1599008
wooldridge12 <wooldridgemikey@gmail.com>
2021-04-09 14:34:54 -0400

    Added new places and started some worked on some JS

--- scripts/database.js
+++ scripts/database.js
@@ -1,9 +1,58 @@
+// const database = {
+//     fish: [
+//         {
+//             name:["Geraldice", "Fishsticks", "Deep Blue", "Mrs Puff", "Nemo",],
+//             species:["angel fish", "Flopper", "Great White", "Puffer fish", "Clown Fish",],
+//             length:["3''", "5'", "20'", "2''", "4''",],
+//             location:["costa rica", "Battle Royal Island", "Mexicos bay califonia", "costa Rica", "In da deep blue sea",],
+//             food:["Tacos", "Bananas", "Blood", "Anything unedible to humans", "worms",],
+//         }
+//     ]
+// }
 const database = {
     fish: [
         {
-            name:"",
-            food:"",
-            size:"",
+            name:"Geraldice",
+            image:"https://tailandfur.com/wp-content/uploads/2015/06/10-Most-Colorful-Fresh-Water-Fish-6.jpg",
+            species:"Angelfish",
+            length:"3''",
+            location:"Costa Rica",
+            food:"Tacos",
+        },
+        {
+            name:"Fishsticks",
+            image:"https://progameguides.com/wp-content/uploads/2018/12/fortnite-outfit-fishstick-1.jpg",
+            species:"Flopper",
+            length:"5'",
+            location:"Batlle Royal Island",
+            food:"Bananas",
+        },
+        {
+            name:"Deep Blue",
+            image:"https://www.surfertoday.com/images/stories/deepblueramsey.jpg",
+            species:"Greatwhite",
+            length:"20'",
+            location:"Mexico's Baja California",
+            food:"Blood",
+        },
+        {
+            name:"Mrs Puff",
+            image:"http://www.crew-center.com/sites/default/files/gallery/3710/5683.jpg",
+            species:"Pufferfish",
+            length:"2''",
+            location:"Costa Rica",
+            food:"Anything None edible to humans",
+        },
+        {
+            name:"Nemo",
+            image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG8G-WR2ZlRdLdujjw09Nfg-NGArcKf0ZtxQ&usqp=CAU",
+            species:"Clown Fish",
+            length:"4''",
+            location:"In da Deep Blue Sea",
+            food:"worms",
         }
     ]
+}
+function createList (fishObjects){
+    return fishObjects;
 }
